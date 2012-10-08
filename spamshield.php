72abc43f9b37022d0e89bc6b9d5aabea084d200b
Panagiotis Papadomitsos <pj@ezgr.net>
2012-10-08 22:23:15 +0300

    cosmetic fixes

--- spamshield.php
+++ spamshield.php
@@ -74,13 +74,13 @@ $mailsubj = 'SpamShield report for server '.HOST; // Set here the mail subject y
 
 $mail = new PHPMailer(true);
 $mail->IsSMTP();
-$mail->SMTPDebug  	= 0;		 // enables SMTP debug information (for testing)
-$mail->SMTPAuth   	= true;	  // enable SMTP authentication
-$mail->Host	   		= $mailhost; // sets the SMTP server
-$mail->Port	   		= $mailport; // set the SMTP port for the GMAIL server
+$mail->SMTPDebug 	= 0;		 // enables SMTP debug information (for testing)
+$mail->SMTPAuth 	= true;	  // enable SMTP authentication
+$mail->Host 		= $mailhost; // sets the SMTP server
+$mail->Port			= $mailport; // set the SMTP port for the GMAIL server
 $mail->Username   	= $mailuser; // SMTP account username
-$mail->Password   	= $mailpass; // SMTP account password
-$mail->CharSet		= 'utf-8';
+$mail->Password 	= $mailpass; // SMTP account password
+$mail->CharSet 		= 'utf-8';
 $mail->SetFrom($mailfrom, $mailname);
 $mail->Subject = $mailsubj;
 
